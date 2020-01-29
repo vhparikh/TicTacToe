2072d1cf0b96bbb420d01a6c9312e28fa6e3d560
vhparikh <42686619+vhparikh@users.noreply.github.com>
2020-01-29 08:24:03 -0800

    bye hi

--- app/src/main/java/com/example/vp/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/vp/tictactoe/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.vp.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-//hi
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
